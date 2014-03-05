edf99b3c2ef1a53f560f3c237322e02ed5626923
Ahmed <ahmedsalam.2008@gmail.com>
2014-03-05 00:57:48 +0000

    CPU Utilization test file

--- src/ece1779/ec2/InstanceMetrics.java
+++ src/ece1779/ec2/InstanceMetrics.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Vector;
 
@@ -28,11 +29,12 @@ public class InstanceMetrics extends HttpServlet {
         PrintWriter out = response.getWriter();
         out.println("<html>");
         out.println("<head>");
-        out.println("<title>Env Variables</title>");
+        out.println("<title>Instance Metrics</title>");
         out.println("</head>");
         out.println("<body>");
   
-        cloudWatchExample(out);
+        //cloudWatchExample(out);
+        getCPUUtilization(out);
         
         out.println("</body>");
         out.println("</html>");
@@ -99,4 +101,87 @@ public class InstanceMetrics extends HttpServlet {
         }
     }
     
+    void getCPUUtilization(PrintWriter out) throws IOException {
+    	
+    	BasicAWSCredentials awsCredentials = (BasicAWSCredentials)getServletContext().getAttribute("AWSCredentials");
+
+        AmazonCloudWatch cw = new AmazonCloudWatchClient(awsCredentials);
+
+    	/* print instance entries */
+    	try {
+    		/* print table header */
+        	out.println("<table border='1'");
+        	out.println("<tr bgcolor='silver'>");
+        	out.println("<td> Instance ID</td>");
+        	out.println("<td> CPU utilization</td>");
+        	out.println("</tr>");
+        	out.println("</table>");
+    		
+    		/* Filter for instances running our AMI */
+    		List<DimensionFilter> amiFilter = new ArrayList<DimensionFilter>();
+    		amiFilter.add(new DimensionFilter().withName("ImageId").withValue("ami-01f4cf68"));
+    		
+    		ListMetricsRequest listMetricsRequest = new ListMetricsRequest();
+        	listMetricsRequest.setMetricName("CPUUtilization");
+        	listMetricsRequest.setNamespace("AWS/EC2");
+        	listMetricsRequest.setDimensions(amiFilter);
+
+        	ListMetricsResult result = cw.listMetrics(listMetricsRequest);
+        	java.util.List<Metric> 	metrics = result.getMetrics();
+
+        	for (Metric metric : metrics) {
+        		String namespace = metric.getNamespace();
+        		String metricName = metric.getMetricName();
+        		List<Dimension> dimensions = metric.getDimensions();
+        		
+            	GetMetricStatisticsRequest statisticsRequest = new GetMetricStatisticsRequest();
+            	statisticsRequest.setNamespace(namespace);
+            	statisticsRequest.setMetricName(metricName);
+            	statisticsRequest.setDimensions(dimensions);
+
+            	Date endTime = new Date();
+            	Date startTime = new Date(0);
+            	startTime.setTime(endTime.getTime()-1200000); /* 2 minutes */
+
+            	statisticsRequest.setStartTime(startTime);
+            	statisticsRequest.setEndTime(endTime);
+            	statisticsRequest.setPeriod(60); /* 60 second granulatiry */
+            	Vector<String>statistics = new Vector<String>();
+            	statistics.add("Maximum");
+            	statisticsRequest.setStatistics(statistics);
+            	GetMetricStatisticsResult stats = cw.getMetricStatistics(statisticsRequest);
+            	
+            	/* get the latest CPU timestamp */
+            	List<Datapoint> dataPoints = stats.getDatapoints();
+            	Date latestTime = new Date(0);
+            	Datapoint latestPoint = null;
+            	for (Datapoint dataPoint : dataPoints) {
+            		if (dataPoint.getTimestamp().after(latestTime)) {
+            			latestTime = dataPoint.getTimestamp();
+            			latestPoint = dataPoint;
+            		}
+            	}
+
+            	out.println("<p> Namespace: " + namespace + " MetricName: " + metricName + " </p>");
+            	out.print("<p> Dimensions: " + dimensions + " </p>");
+            	out.print("<p> Values: " + stats.toString() + " </p>");
+            	out.print("<p> Latest CPU: " + latestPoint.getMaximum() + " </p>");
+            	out.println("<br>");
+        	}
+
+        } catch (AmazonServiceException ase) {
+            out.println("Caught an AmazonServiceException, which means your request made it "
+                    + "to Amazon EC2, but was rejected with an error response for some reason.");
+            out.println("Error Message:    " + ase.getMessage());
+            out.println("HTTP Status Code: " + ase.getStatusCode());
+            out.println("AWS Error Code:   " + ase.getErrorCode());
+            out.println("Error Type:       " + ase.getErrorType());
+            out.println("Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            out.println("Caught an AmazonClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with EC2, "
+                    + "such as not being able to access the network.");
+            out.println("Error Message: " + ace.getMessage());
+        }
+    }
 }
